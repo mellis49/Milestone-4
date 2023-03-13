9842bbfa3e062c48ba0aa6e05279253156684542
mellis49 <123321925+mellis49@users.noreply.github.com>
2023-03-12 19:56:09 -0500

    Add files via upload

---
+++ JSON_.java
@@ -0,0 +1,64 @@
+package Milestone4_;
+
+public  class JSON_ {
+String Name;
+
+String Description;
+
+int Price;        
+ 
+int Inventory;
+
+
+
+public String getName() {
+	return Name;
+	}
+public void setName(String Name) {
+		this.Name = Name;
+		
+	}
+
+
+
+public String getDescription() {
+	return Description;
+	}
+public void setDescription(String Description) {
+		this.Description = Description;
+		
+	}
+
+
+
+
+
+
+public int getPrice() {
+	return Price;
+	}
+public void setPrice(int Price) {
+		this.Price = Price;
+		
+	}
+
+
+
+public int getInventory() {
+	return Inventory;
+	}
+public void setInventory(int Inventory) {
+		this.Inventory = Inventory;
+		
+	}
+
+
+
+
+
+
+
+
+  }
+
+
